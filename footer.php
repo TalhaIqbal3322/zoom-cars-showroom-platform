3dee094132aa772844898d8d2494f9335696d4ba
Talha-Iqbal293 <talhaiqbal.digitalnexus@gmail.com>
2025-06-05 00:05:40 +0500

    feat(layout): add dynamic and reusable footer component

---
+++ footer.php
@@ -0,0 +1,25 @@
+<footer>
+    <div class="footer-container">
+        <div class="contact-info">
+            <div class="heading contact-heading">
+                <i class="fas fa-address-card"></i>
+                <h2>Visit Us</h2>
+            </div>
+            <p>DUCAMAZ AUTO MARKET, SHOWROOM NO.222</p>
+            <p>RAS AL KHOR, Dubai</p>
+            <p>United Arab Emirates</p>
+        </div>
+        <div class="google-map">
+            <div class="heading">
+                <i class="fas fa-map-marker-alt"></i>
+                <h2>Find Us Here</h2>
+            </div>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3610.962030835499!2d55.377141210605664!3d25.170759877635128!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f6707d905a8f5%3A0x1d7c719759ac9aec!2sZoom%20Cars%20FZE!5e0!3m2!1sen!2s!4v1719361215316!5m2!1sen!2s" width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+    </div>
+    <div class="footer-bottom">
+        <p>Copyright © 2024 Zoom Cars. Site By (<a href="https://wa.me/+923261194293" target="blank" style="color: black;text-decoration: none;">+92 326 1194293</a>)</p>
+        <!-- <hr> -->
+        <!-- <a href="Admin/" style="font-size: 12px;text-decoration: none;color: black;">Continue as Admin</a> -->
+    </div>
+</footer>
