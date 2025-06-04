90bf0e7d5699f6dcc9bba83af0ffc9aef0239db7
Talha-Iqbal293 <talhaiqbal.digitalnexus@gmail.com>
2025-06-05 00:05:31 +0500

    chore(db): configure database connection using PHP

---
+++ db.php
@@ -0,0 +1,6 @@
+<?php
+$conn = mysqli_connect("localhost", "zoomcarsfze_admin", "w?SS&FdH-hEJ", "zoomcarsfze_zoom_motors");
+
+if (mysqli_connect_errno()) {
+    echo "Failed to connect: " . mysqli_connect_errno();
+}
