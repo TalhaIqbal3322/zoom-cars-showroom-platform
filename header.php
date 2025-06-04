d374a9e97ef2c18efd222c9ca6c656e52ccedbf8
Talha-Iqbal293 <talhaiqbal.digitalnexus@gmail.com>
2025-06-05 00:06:00 +0500

    feat(layout): implement header with responsive navigation

---
+++ header.php
@@ -0,0 +1,58 @@
+<header>
+    <div class="top-bar bg-primary text-white py-2">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-6 col-md-6 col-sm-12 d-flex justify-content-center align-items-center">
+                    <div class="top-info text-center">
+                        <img src="pics/time.png" alt="Clock Icon" class="top-icon me-2">
+                        <span>Mon-Sat: 9:00 AM - 7:00 PM | Dubai, UAE</span>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-12 d-flex justify-content-end align-items-center">
+                    <div class="top-services">
+                        <a href="https://www.facebook.com/profile.php?id=61561208494131" target="_blank" class="text-white me-2">
+                            <img src="pics/facebook.png" alt="Facebook" class="img-fluid">
+                        </a>
+                        <a href="https://www.instagram.com/zoomcarsdubai?igsh=MWNhazVxczJkaWUzNg==" target="_blank" class="text-white me-2">
+                            <img src="pics/instgram.png" alt="Instagram" class="img-fluid">
+                        </a>
+                        <a href="https://www.tiktok.com/@zoomcarsfze?lang=en" target="_blank" class="text-white me-2">
+                            <img src="pics/tiktok.png" alt="TikTok" class="img-fluid">
+                        </a>
+                        <a href="https://wa.me/971563078782" target="_blank" class="text-white me-2">
+                            <img src="pics/whatsapp.png" alt="WhatsApp" class="img-fluid">
+                        </a>
+                        <a href="mailto:nadeempak222@yahoo.com" class="text-white">
+                            <img src="pics/gmail.png" alt="Gmail" class="img-fluid">
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php"><img src="pics/logo.png" alt="Logo" class="img-fluid" style="height: 75px;"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a href="index.php" class="nav-link" aria-current="page"><img src="pics/home.png" class="nav-icon me-1"> Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="about.php" class="nav-link"><img src="pics/about.png" class="nav-icon me-1"> About Us</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="all-products.php" class="nav-link"><img src="pics/services.webp" class="nav-icon me-1"> Our Products</a>
+                    </li>
+                </ul>
+                <div class="d-flex">
+                    <a href="tel:+971563078782" class="btn btn-outline-light"><img src="pics/call now.png" class="nav-icon me-2">Call Now: +971563078782</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+</header>
